be8afafcb2e85920f25a4b36fe3e3cc95c2a2405
TaiKhau <lekhauhuutai48@gmail.com>
2024-12-01 20:07:12 +0700

    sua lai dockerfile va dockercompose

--- script.sh
+++ script.sh
@@ -6,14 +6,6 @@ sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
 
-#install jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-
 #install docker
 sudo apt-get update
 sudo apt-get install docker.io -y
@@ -21,7 +13,6 @@ sudo usermod -aG docker ubuntu
 sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
-sudo systemctl restart jenkins
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 #install docker compose
@@ -41,12 +32,6 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
-
-# Install Node.js 16 and npm
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update
-sudo apt install -y nodejs
 
 # Install Terraform
 sudo apt install wget -y

4928d05ba5050b40fd5b4a13a87c50c31f9ea6da
TaiKhau <lekhauhuutai48@gmail.com>
2024-12-03 00:20:21 +0700

    lai fix j do

--- frontend/src/main/java/com/example/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/example/frontend/FrontendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public lass FrontendApplication {
+public class FrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
